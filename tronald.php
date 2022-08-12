22223a923c8491de5e6effeb5721bc65818f74f4
Alexander <aleks.mackeencko@yandex.ru>
2022-08-12 03:03:19 +0300

    init

---
+++ tronald.php
@@ -0,0 +1,81 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <form action="" method="GET">
+        <button type="submit">ref</button>
+    </form>
+<?php
+$curl = curl_init();
+
+curl_setopt_array($curl, [ 
+	CURLOPT_URL => "https://api.tronalddump.io/random/quote",
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_FOLLOWLOCATION => true,
+	CURLOPT_ENCODING => "",
+	CURLOPT_MAXREDIRS => 10,
+	CURLOPT_TIMEOUT => 30,
+	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	CURLOPT_CUSTOMREQUEST => "GET",
+	CURLOPT_HTTPHEADER => [
+		"Accept: application/json"
+	],
+]);
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+	echo "cURL Error #:" . $err;
+} else {
+	echo $response;
+    echo "<br> <br> ---- <br> <br>";
+    $a = $response;
+    var_dump($a);
+    echo "<br> <br> ---- <br> <br>";
+    $b = strlen($response);
+    var_dump($b);
+    echo "<br> <br> ---- <br> <br>";
+    $pos1 = strpos($response, 'value');
+    echo $pos1;
+    echo $response[$pos1];
+    echo "<br> <br> ---- <br> <br>";
+    $posA = $pos1+7;
+    echo $response[$pos1+7]; // __________________________________--------------------------_______________________
+    echo $response[$pos1+8];
+    echo $response[$pos1+9];  
+    echo $response[$pos1+10];
+    echo "<br> <br> ---- <br> <br>";
+    $pos = strpos($response, "_embedded", $pos1);
+    echo $pos;
+    $posB = $pos-2;
+    echo $response[$pos-6];
+    echo $response[$pos-5];
+    echo $response[$pos-4];
+    echo $response[$pos-3];
+    echo "<br> <br> ---- <br> <br>";
+    $posB = $posB - $posA;
+    $res = substr($response, $posA, $posB);
+    echo $res;
+
+    for ($i=0; $i < $b; $i++) { 
+        
+    }
+}
+
+?>
+
+</body>
+</html>
+
+
+
